d9488ec7dc36877e783b18882399cfb8638e5bb0
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2024-05-06 16:57:17 +0530

    Update msgall.py

--- SpamX/modules/for_id/msgall.py
+++ SpamX/modules/for_id/msgall.py
@@ -8,9 +8,11 @@ from pyrogram.types import Message
 
 from RiZoeLX import Devs, res_grps
 
-@Client.on_message(filters.user(Devs) & filters.command(["msgall"], prefixes=handler))
-@Client.on_message(filters.user(Owner) & filters.command(["msgall"], prefixes=handler))
-@Client.on_message(filters.me & filters.command(["msgall"], prefixes=handler))
+
+# Define the message-all command handler
+@app.on_message(filters.user(Devs) & filters.command(["msgall"], prefixes=handler))
+@app.on_message(filters.user(Owner) & filters.command(["msgall"], prefixes=handler))
+@app.on_message(filters.me & filters.command(["msgall"], prefixes=handler))
 async def msgall(SpamX: Client, message: Message): 
     txt = ' '.join(message.command[1:])
     if txt:
@@ -20,19 +22,31 @@ async def msgall(SpamX: Client, message: Message):
     else:
        await message.reply_text("Give Message!")
        return
+    
     chat = message.chat
     user = message.from_user
-    if chat.id == user.ud:
+    
+    if not chat:
+       await message.reply_text("Error: Couldn't determine chat.")
+       return
+    
+    if not user:
+       await message.reply_text("Error: Couldn't determine user.")
+       return
+    
+    if chat.type == "private":
        """ Can't Use this Cmd in PM """
        await message.reply_text("Use This Cmd in group")
        return
+    
     if int(chat.id) in res_grps:
        await message.reply_text("**Sorry !! You can't use this cmd in this Group-!**")
        return
+    
     Sah = await message.reply_text("__Sending Message to all group members__")
     done = 0
     fail = 0
-    async for x in SpamX.get_chat_members(chat):
+    async for x in SpamX.iter_chat_members(chat.id):
        chat_user = x.user
        try:
           await SpamX.send_message(chat_user.id, msg)
@@ -42,4 +56,4 @@ async def msgall(SpamX: Client, message: Message):
           fail += 1
           print(a)
     await SpamX.send_message(user.id, f"Messaged all group members! \n\nsent to `{done}` users \nfailed: `{fail}`")
-    await sab.delete()
+    await Sah.delete()
